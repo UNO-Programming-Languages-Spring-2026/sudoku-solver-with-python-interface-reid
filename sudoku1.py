71ad4be32ab37208d64e9fe2b95b71a08b022a53
Reid Chapman <rmchapman2004@gmail.com>
2026-03-24 13:08:46 -0500

    Completed Question 1a

--- sudoku1.py
+++ sudoku1.py
@@ -2,11 +2,12 @@ import clingo
 
 class ClingoApp(clingo.application.Application):
     def main(self, ctl, files):
+        ctl.load("sudoku2.lp")
         for f in files:
             ctl.load(f)
         if not files:
             ctl.load("-")
-        ctl.ground()
+        ctl.ground([("base", [])])
         ctl.solve()
 
 
