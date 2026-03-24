3d2a368c3f6ca95b8c2be8a83ed32719992e4006
Reid Chapman <rmchapman2004@gmail.com>
2026-03-24 12:37:35 -0500

    Created Sudoku 1 main function

---
+++ sudoku1.py
@@ -0,0 +1,13 @@
+import clingo
+
+class ClingoApp(clingo.application.Application):
+    def main(self, ctl, files):
+        for f in files:
+            ctl.load(f)
+        if not files:
+            ctl.load("-")
+        ctl.ground()
+        ctl.solve()
+
+
+clingo.application.clingo_main(ClingoApp())
