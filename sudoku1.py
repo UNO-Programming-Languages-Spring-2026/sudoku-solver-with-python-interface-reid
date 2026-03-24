439acb140ce3f9341f8f3e57f099cda1c307c978
Reid Chapman <rmchapman2004@gmail.com>
2026-03-24 13:10:48 -0500

    Completed Question 1b

--- sudoku1.py
+++ sudoku1.py
@@ -1,6 +1,12 @@
-import clingo
+import clingo, sys
 
 class ClingoApp(clingo.application.Application):
+    
+    def print_model(self, model, printer) -> None:
+        symbols = sorted(model.symbols(shown=True))
+        print(" ".join(str(s) for s in symbols))
+        sys.stdout.flush()
+
     def main(self, ctl, files):
         ctl.load("sudoku2.lp")
         for f in files:
